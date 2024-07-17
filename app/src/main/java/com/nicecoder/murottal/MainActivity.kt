bb0979ce6118e3c56bcb08c2716a5aa7e2c1e87c
dody <do@nicecoder.com>
2024-07-17 09:06:26 +0700

    clean up

--- app/src/main/java/com/nicecoder/murottal/MainActivity.kt
+++ app/src/main/java/com/nicecoder/murottal/MainActivity.kt
@@ -7,10 +7,7 @@ import android.content.ServiceConnection
 import android.os.Bundle
 import android.os.IBinder
 import android.widget.ListView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
